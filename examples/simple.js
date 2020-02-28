4b267bdf276011ef5dcebaeda89d31c07472be83
langerhans <langerhans@kaltarbeit.com>
2020-02-28 09:44:00 +0900

    test version

--- examples/simple.js
+++ examples/simple.js
@@ -15,10 +15,10 @@ function onVisibleChange(visible) {
 
 const menu = (
   <Menu onSelect={onSelect}>
-    <MenuItem disabled>disabled</MenuItem>
-    <MenuItem key="1">one</MenuItem>
+    <MenuItem key="1"><a href="/">one</a></MenuItem>
     <Divider />
-    <MenuItem key="2">two</MenuItem>
+    <MenuItem key="2"><a href="/">two</a></MenuItem>
+    <MenuItem key="3"><a href="/">three</a></MenuItem>
   </Menu>
 );
 
@@ -27,13 +27,16 @@ ReactDOM.render(
     <div style={{ height: 100 }}/>
     <div>
       <Dropdown
-        trigger={['click']}
+        trigger={['focus']}
         overlay={menu}
         animation="slide-up"
         onVisibleChange={onVisibleChange}
+        getPopupContainer={trigger => trigger}
       >
         <button style={{ width: 100 }}>open</button>
       </Dropdown>
+
+      <button>Close</button>
     </div>
   </div>
 , document.getElementById('__react-content'));
